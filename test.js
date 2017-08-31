9b622dc4d70048705b347ffb70dffcb6ee5257aa
Alexander S <ams_103@hotmail.com>
2017-08-31 14:34:03 +0200

    Add files via upload
    first commit, ar-drone basic 3 point movement

---
+++ test.js
@@ -0,0 +1,42 @@
+var arDrone = require('ar-drone');
+var client = arDrone.createClient();
+
+client.takeoff();
+client
+  .after(5000, function(){
+    this.front(1);
+  })
+  .after(3000, function(){
+    this.stop();
+    this.land();
+  })
+  .after(4000, function(){
+    client.takeoff();
+  })
+  .after(5000, function(){
+    this.counterClockwise(0.3);
+  })
+  .after(3000, function(){
+    this.stop();
+    this.land();
+  })
+
+  .after(4000, function(){
+    client.takeoff();
+  })
+  /*
+  .after(3000, function(){
+    this.animate('flipBehind', 1000);
+  })
+  .after(1000, function(){
+    this.stop();
+    this.land();
+  })*/
+
+  .after(4000, function(){
+    this.front(0.2);
+  })
+  .after(2500, function(){
+    this.stop();
+    this.land();
+  });
